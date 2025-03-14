7cb3c7dec761fd5799483895a9fd84075e117c67
Dalila <dalila.fariello@gmail.com>
2025-03-14 16:04:38 +0100

    fix: update text in Contact page

--- EventPlanner/src/pages/Contact.jsx
+++ EventPlanner/src/pages/Contact.jsx
@@ -7,8 +7,8 @@ const Contact = () => {
       <div className="contact-me-info">
         <h2 className="contact-h2">Me contacter</h2>
         <p className="contact-paragraph">
-          Pour toute demande de renseignements sur les événements ou pour le
-          contact presse, n’hésitez pas à me contacter ici.
+          Pour toute demande de renseignements sur les événements ou pour toute
+          autre demande, n’hésitez pas à me contacter ici.
         </p>
       </div>
       <ContactForm />
